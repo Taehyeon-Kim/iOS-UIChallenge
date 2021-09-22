71c8daa442d0cda0086ae0eb25305fc946ae251f
김태현 <taehyk528@gmail.com>
2021-09-22 22:05:40 +0900

    ➕[ADD] : 시작점 변경 (KakaoNoticeVC)

--- UIChallenge/UIChallenge/App/AppDelegate.swift
+++ UIChallenge/UIChallenge/App/AppDelegate.swift
@@ -13,8 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow()
+        let naviVC = UINavigationController(rootViewController: KakaoNoticeViewController())
+        window.rootViewController = naviVC
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 }
-
