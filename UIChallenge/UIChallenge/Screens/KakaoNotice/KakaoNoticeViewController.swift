6a168c6bbda4cfeabc381c842a36b35d16454456
김태현 <taehyk528@gmail.com>
2021-09-22 21:15:10 +0900

    ➕[ADD] : KakaoNoticeViewController

---
+++ UIChallenge/UIChallenge/Screens/KakaoNotice/KakaoNoticeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  KakaoNoticeViewController.swift
+//  UIChallenge
+//
+//  Created by taehy.k on 2021/09/22.
+//
+
+/*
+ 다음 뷰는 카카오톡 공지 UI를 따라해 본 것입니다.
+ 
+ - Animation
+ - layoutIfNeeded vs setNeedsLayout
+ */
+
+import UIKit
+
+class KakaoNoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
+
+
